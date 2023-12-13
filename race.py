7df6dafe6c73ae7ae77afadbb7c28cbe6a7b8fd7
tyranoporc <81986076+tyranoporc@users.noreply.github.com>
2023-12-13 09:19:02 +0100

    rajout race

--- race.py
+++ race.py
@@ -1,5 +1,25 @@
 from Entites import Entites
-class Race(Entites):
+       
+class gobelin(Entites):
+    def __init__(self):
+        Entites.__init__(self)
+    
+class squelette(Entites):
+    def __init__(self):
+        Entites.__init__(self)
+    
+class orc(Entites):
+    def __init__(self):
+        Entites.__init__(self)
+    
+class vampire(Entites):
+    def __init__(self):
+        Entites.__init__(self)
+    
+class elf_noir(Entites):
     def __init__(self):
         Entites.__init__(self)
         
+class trouver_race_archer(Entites):
+    def __init__(self):
+        Entites.__init__(self)
